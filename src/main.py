bec184562706436bca9c7f77c5d607ffb4d57c98
PavelSBvt <SPS260951@yandex.ru>
2026-09-25 15:43:08 +0300

    > [py] change main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
-from core.user_data_entry import EntryData
+from core.user_data_entry import UserEntryReportData
 from utils.output_rich import Rich
 
 
 if __name__ == "__main__":
+    Rich.print_spacer()
     Rich.simple_log("Запуск программы")
-    EntryData.entry_report_data()
+    Rich.print_spacer()
+
+    EntryReportData = UserEntryReportData()
+
+    EntryReportData.entry_report_data()
+    EntryReportData.save_data_to_csv()
+    EntryReportData.get_csv_data()
