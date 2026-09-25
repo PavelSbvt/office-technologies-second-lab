f067946a7a030fddf7b3ea36fd7700e33bd972f2
PavelSBvt <SPS260951@yandex.ru>
2026-09-25 22:50:25 +0300

    > [py] create new func for print table with data on Rich and change all methods to static

--- src/utils/output_rich.py
+++ src/utils/output_rich.py
@@ -1,18 +1,20 @@
 from rich.console import Console
+from rich.table import Table
 
 
 console = Console(width=200)
 
 class RichLog:
     """
-    Класс с методами вывода логов в консоль с использованием библиотеки Ruch
+    Класс с методами вывода логов в консоль с использованием библиотеки Rich
     """
 
     def __init__(self):
         pass
 
 
-    def error_log(self, message: str) -> None:
+    @staticmethod
+    def error_log(message: str) -> None:
         """
         Лог об ошибке (используется при сообщении об ошибке выполнении
         какой-то операции) - вывод в терминал (с использованием библиотеки Rich)
@@ -26,7 +28,8 @@ class RichLog:
         console.log(f"[red][ERROR] {message}[/red]", _stack_offset=2)
 
 
-    def success_log(self, message: str) -> None:
+    @staticmethod
+    def success_log(message: str) -> None:
         """
         Лог об успешном выполнении (используется при сообщении об успешном выполнении
         какой-то операции) - вывод в терминал (с использованием библиотеки Rich)
@@ -40,7 +43,8 @@ class RichLog:
         console.log(f"[green][SUCCESS] {message}[/green]",  _stack_offset=2)
 
 
-    def simple_log(self, message: str) -> None:
+    @staticmethod
+    def simple_log(message: str) -> None:
         """
         Обычный лог (информационный) - вывод в терминал (с использованием библиотеки Rich)
 
@@ -53,11 +57,12 @@ class RichLog:
         console.log(f"[cyan][INFO] {message}[/cyan]",  _stack_offset=2)
 
 
-    def debug_log(self, message: str) -> None:
+    @staticmethod
+    def debug_log(message: str) -> None:
         """
-        Лог для дебаггинга - вывод в терминал (с использованием библиотеки Rich)
+        Лог для логирования - вывод в терминал (с использованием библиотеки Rich)
 
-        :param message: Принимает тест дебаг лога, который будет выведен в терминал (консоль)
+        :param message: Принимает текст лога, который будет выведен в терминал (консоль)
 
         :return: None
         """
@@ -65,7 +70,8 @@ class RichLog:
         console.log(f"[grey46][Debug] {message}[/grey46]",  _stack_offset=2)
 
 
-    def warning_log(self, message: str) -> None:
+    @staticmethod
+    def warning_log(message: str) -> None:
         """
         Лог-предупреждение о каких-то незначительных проблемах выполнения операции
          - вывод в терминал (с использованием библиотеки Rich)
@@ -78,7 +84,8 @@ class RichLog:
         console.log(f"[bold yellow][WARN] ВНИМАНИЕ: {message}[/bold yellow]",  _stack_offset=2)
 
 
-    def print_spacer(self) -> None:
+    @staticmethod
+    def print_spacer() -> None:
         """
         Разделитель для более наглядного показа лога процессов в консоли
 
@@ -88,7 +95,8 @@ class RichLog:
         console.log(f"[grey46][Debug] {"-" * 151}[/grey46]", _stack_offset=2)
 
 
-    def print_spacer_points(self) -> None:
+    @staticmethod
+    def print_spacer_points() -> None:
         """
         Разделитель для более наглядного показа лога процессов в консоли -
         разделитель в виде точек
@@ -99,7 +107,8 @@ class RichLog:
         console.log(f"[grey46][Debug] {"." * 151}[/grey46]", _stack_offset=2)
 
 
-    def input_data(self, input_label: str):
+    @staticmethod
+    def input_data(input_label: str):
         """
         Функция для красивого оформления ввода данных через терминал с использованием
          библиотеки Rich
@@ -111,6 +120,29 @@ class RichLog:
         value = console.input(f"[bold orange1][INPUT] {input_label}: [/bold orange1]")
 
         return value
+
+
+    @staticmethod
+    def print_tabel(title: str, cols: list, rows: list) -> None:
+        """
+        Функция для вывода таблицы в консоль с помощью библиотеки Rich
+
+        :param title: Str - Принимает заголовок таблицы
+        :param cols: list - Принимает список с содержимым столбцов
+        :param rows: list - Принимает список с содержимым строк
+
+        :return: None
+        """
+
+        table = Table(title=title)
+
+        for col in cols:
+            table.add_column(col)
+
+        for row in rows:
+            table.add_row(*[str(cell) for cell in row])
+
+        Console().print(table)
 
 
 Rich = RichLog()
