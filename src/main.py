ce05c75b5a3ef14d8412af0b6fe2113e897684e3
PavelSBvt <SPS260951@yandex.ru>
2026-09-25 15:33:38 +0300

    > [py] betterment code design

--- src/main.py
+++ src/main.py
@@ -6,7 +6,9 @@ if __name__ == "__main__":
     Rich.print_spacer()
     Rich.simple_log("Запуск программы")
     Rich.print_spacer()
+
     EntryReportData = UserEntryReportData()
+
     EntryReportData.entry_report_data()
     EntryReportData.save_data_to_csv()
     EntryReportData.get_csv_data()
